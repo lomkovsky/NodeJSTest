e93410402199563328eb212055c968d4ccde0daf
dima.dushkin <dima.dushkin@room4.team>
2020-05-08 12:10:20 +0300

    added dotenv module

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const fs = require('fs');
 const CryptoJS = require("crypto-js");
 const http = require('http');
@@ -17,15 +18,15 @@ wss.on('connection', ws => {
     } else {
       wss.clients.forEach(client => {
         if (client.readyState === Websocket.OPEN) {
-          const messageEncrypt = CryptoJS.AES.encrypt(message, 'TEST').toString();
+          const messageEncrypt = CryptoJS.AES.encrypt(message, process.env.SECRET).toString();
           client.send(messageEncrypt);
         }
       });
     }
   });
-  const messageEncrypt = CryptoJS.AES.encrypt('Welcome message!', 'TEST').toString();
+  const messageEncrypt = CryptoJS.AES.encrypt('Welcome message!', process.env.SECRET).toString();
   ws.send(messageEncrypt);
 });
-server.listen(3001, () => {
-  console.log('Listen port 3001');
+server.listen(process.env.PORT, () => {
+  console.log(`Listen port ${process.env.PORT}`);
 });
